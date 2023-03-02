fc5958b71b27030433c6366cae5e999989099d57
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-03-02 17:56:51 +0100

    update

--- client/src/routes/(app)/+layout.svelte
+++ client/src/routes/(app)/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 </script>
 
-<navigation class="flex flex-row items-center justify-center gap-2 text-xl mb-10 text-emerald-400">
+<navigation
+    class="flex flex-row items-center justify-center gap-2 text-xl mb-10 text-emerald-400"
+>
     <a
         href="/"
+        class="border rounded p-2 border-emerald-400 hover:border-emerald-500"
+    >
+        Home
+    </a>
+
+    <a
+        href="/notes"
         class="border rounded p-2 border-emerald-400 hover:border-emerald-500"
     >
         Notes
