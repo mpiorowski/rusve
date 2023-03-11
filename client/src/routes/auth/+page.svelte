37441d776a449680061d5b1d2b0d4d4bdc07317a
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-03-11 11:07:11 +0100

    update

--- client/src/routes/auth/+page.svelte
+++ client/src/routes/auth/+page.svelte
@@ -8,11 +8,11 @@
 </script>
 
 <div
-    class="h-screen w-screen flex flex-col items-center justify-center max-w-lg m-auto px-6"
+    class="flex flex-col items-center justify-center max-w-lg m-auto px-6"
 >
     <button
         on:click={onSignInUsingGoogle}
-        class="flex flex-row gap-4 justify-center items-center border rounded p-2 border-gray-300 bg-gray-300 text-gray-900"
+        class="flex flex-row gap-4 justify-center items-center border rounded px-4 py-2 border-gray-300 bg-gray-300 text-gray-900"
     >
         <div class="h-8">
             <Gmail />
