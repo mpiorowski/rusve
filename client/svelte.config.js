f1eda5dce9f358bd5051d4e2fec6942fac3e4c9e
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-03-24 02:14:32 +0100

    update

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-node';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
