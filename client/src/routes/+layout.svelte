7b3d36277af85e30ec489fff282fedcfdc989893
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-04-28 22:39:40 +0200

    update

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -4,6 +4,14 @@
     import { toastStore } from "$lib/toast/toast";
 </script>
 
+<svelte:head>
+    <title>Rusve</title>
+    <meta
+        name="description"
+        content="Open source Rust with SvelteKit application, using gRPC and microservices"
+    />
+</svelte:head>
+
 <div class="absolute top-10 right-10 flex flex-col gap-2">
     {#each $toastStore as toast}
         <Toast {toast} />
