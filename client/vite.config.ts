32cb2b59b8ee59cf42daa8a02067b11d2a97f5be
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-05-06 14:56:19 +0200

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 
 export default defineConfig({
-    plugins: [
-        sveltekit(),
-    ],
+    plugins: [sveltekit(), SvelteKitPWA()],
     test: {
         include: ["src/**/*.{test,spec}.{js,ts}"],
     },
