822d20c08b6fc55a761a81b77c281570910da5f8
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-05-06 14:36:59 +0200

    update

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -14,35 +14,9 @@
         });
         window.location.reload();
     }
-
-    import { onMount } from "svelte";
-    import { pwaInfo } from "virtual:pwa-info";
-
-    onMount(async () => {
-        if (pwaInfo) {
-            const { registerSW } = await import("virtual:pwa-register");
-            registerSW({
-                immediate: true,
-                onRegistered(r) {
-                    // uncomment following code if you want check for updates
-                    // r && setInterval(() => {
-                    //    console.log('Checking for sw update')
-                    //    r.update()
-                    // }, 20000 /* 20s for testing purposes */)
-                    console.log(`SW Registered: ${r}`);
-                },
-                onRegisterError(error) {
-                    console.log("SW registration error", error);
-                },
-            });
-        }
-    });
-
-    $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : "";
 </script>
 
 <svelte:head>
-    {@html webManifest}
     <title>Rusve</title>
     <meta
         name="description"
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     plugins: [
         sveltekit(),
-        SvelteKitPWA(),
     ],
     test: {
         include: ["src/**/*.{test,spec}.{js,ts}"],
