c67b675f4f11ead5ed268a6e7148b508ff8205d6
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-05-13 01:18:32 +0200

    update

--- service-notes-rust/src/notes_service.rs
+++ service-notes-rust/src/notes_service.rs
@@ -7,6 +7,7 @@ use futures_util::TryStreamExt;
 use sqlx::{types::Uuid, Row};
 use time::OffsetDateTime;
 use tokio::sync::mpsc;
+use tokio_postgres::Config;
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Request, Response, Status};
 
@@ -79,48 +80,83 @@ impl NotesService for MyService {
         println!("GetNotes = {:?}", request);
         let start = std::time::Instant::now();
 
-        let pool = self.pool.clone();
+        // Create a connection pool with 5 connections
+        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let (client, connection) = tokio_postgres::connect(&database_url, tokio_postgres::NoTls)
+            .await
+            .map_err(|e| Status::internal(e.to_string()))?;
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+
+        // let pool = self.pool.clone();
         let (tx, rx) = mpsc::channel(4);
 
         let user_id = request.into_inner().user_id;
         let user_id = Uuid::parse_str(&user_id).map_err(|e| Status::internal(e.to_string()))?;
 
-        let mut notes_stream = sqlx::query(
-            "SELECT * FROM notes WHERE user_id = $1 and deleted is null order by created desc",
-        )
-        .bind(user_id)
-        .fetch(&pool);
+        let rows = client
+            .query(
+                "SELECT * FROM notes WHERE user_id = $1 and deleted is null order by created desc",
+                &[&user_id],
+            )
+            .await
+            .map_err(|e| Status::internal(e.to_string()))?;
 
         tokio::spawn(async move {
             println!("Prepare: {:?}", start.elapsed());
 
-            let start_loop = std::time::Instant::now();
-
-            loop {
-                match notes_stream.try_next().await {
-                    Ok(None) => {
-                        let elapsed = start.elapsed();
-                        println!("Elapsed: {:.2?}", elapsed);
-                        println!("Loop: {:.2?}", start_loop.elapsed());
-                        break;
-                    }
-                    Ok(note) => {
-                        let note: Note = match note.try_into() {
-                            Ok(note) => note,
-                            Err(e) => {
-                                tx.send(Err(Status::internal(e.to_string()))).await.unwrap();
-                                break;
-                            }
-                        };
-                        tx.send(Ok(note)).await.unwrap();
-                    }
-                    Err(e) => {
-                        println!("Error: {:?}", e);
-                        tx.send(Err(Status::internal(e.to_string()))).await.unwrap();
-                        break;
-                    }
-                }
+            for row in rows {
+                let pg_note = PgNote {
+                    id: row.get("id"),
+                    user_id: row.get("user_id"),
+                    title: row.get("title"),
+                    content: row.get("content"),
+                    created: row.get("created"),
+                    updated: row.get("updated"),
+                    deleted: row.get("deleted"),
+                };
+                let note = Note {
+                    id: pg_note.id.to_string(),
+                    user_id: pg_note.user_id.to_string(),
+                    title: pg_note.title,
+                    content: pg_note.content,
+                    created: pg_note.created.to_string(),
+                    updated: pg_note.updated.to_string(),
+                    deleted: pg_note.deleted.map(|d| d.to_string()),
+                    user: None,
+                };
+                tx.send(Ok(note)).await.unwrap();
             }
+            let elapsed = start.elapsed();
+            println!("Elapsed: {:.2?}", elapsed);
+            // loop {
+            //     match notes_stream.try_next().await {
+            //         Ok(None) => {
+            //             let elapsed = start.elapsed();
+            //             println!("Elapsed: {:.2?}", elapsed);
+            //             println!("Loop: {:.2?}", start_loop.elapsed());
+            //             break;
+            //         }
+            //         Ok(note) => {
+            //             let note: Note = match note.try_into() {
+            //                 Ok(note) => note,
+            //                 Err(e) => {
+            //                     tx.send(Err(Status::internal(e.to_string()))).await.unwrap();
+            //                     break;
+            //                 }
+            //             };
+            //             tx.send(Ok(note)).await.unwrap();
+            //         }
+            //         Err(e) => {
+            //             println!("Error: {:?}", e);
+            //             tx.send(Err(Status::internal(e.to_string()))).await.unwrap();
+            //             break;
+            //         }
+            //     }
+            // }
         });
         Ok(Response::new(ReceiverStream::new(rx)))
     }
@@ -136,15 +172,13 @@ impl NotesService for MyService {
         let user_id =
             Uuid::parse_str(&note.user_id).map_err(|e| Status::internal(e.to_string()))?;
 
-        sqlx::query(
-            "INSERT INTO notes (title, content, user_id) VALUES ($1, $2, $3) RETURNING *",
-        )
-        .bind(note.title.clone())
-        .bind(note.content.clone())
-        .bind(user_id)
-        .fetch_one(&pool)
-        .await
-        .map_err(sqlx::Error::into_status)?;
+        sqlx::query("INSERT INTO notes (title, content, user_id) VALUES ($1, $2, $3) RETURNING *")
+            .bind(note.title.clone())
+            .bind(note.content.clone())
+            .bind(user_id)
+            .fetch_one(&pool)
+            .await
+            .map_err(sqlx::Error::into_status)?;
 
         // commit transaction
         tx.commit().await.map_err(sqlx::Error::into_status)?;
