9b679628f29bb0231c1efa04328862ba5d721771
mateuszpiorowski <mateuszpiorowski@gmail.com>
2024-01-03 11:29:05 +0100

    update

--- service-utils/src/email_service.rs
+++ service-utils/src/email_service.rs
@@ -1,10 +1,4 @@
 use anyhow::Result;
-use futures_util::StreamExt;
-use google_cloud_default::WithAuthExt;
-use google_cloud_pubsub::{
-    client::{Client, ClientConfig},
-    subscriber::ReceivedMessage,
-};
 use sendgrid::{Destination, Mail, SGClient};
 use serde::Deserialize;
 
@@ -30,75 +24,5 @@ async fn send_email(message: &ReceivedMessage) -> Result<()> {
         .add_html(email_message.message.as_str());
 
     sg.send(mail_info).await?;
-    Ok(())
-}
-
-pub async fn subscribe_to_emails() -> Result<()> {
-    let env = std::env::var("ENV").unwrap();
-    if env == "development" {
-        return Ok(());
-    }
-    // Create pubsub client.
-    let config = ClientConfig::default().with_auth().await?;
-    let client = Client::new(config).await?;
-    let subscription = client.subscription("email-sub");
-
-    // Check if subscription exists.
-    if !subscription.exists(None).await? {
-        return Err(anyhow::anyhow!("Subscription does not exist"));
-    }
-
-    // Token for cancel.
-    let cancel = CancellationToken::new();
-
-    tokio::spawn(async move {
-        let mut stream = match subscription.subscribe(None).await {
-            Ok(stream) => stream,
-            Err(e) => {
-                tracing::error!("Error subscribing to pubsub: {:?}", e);
-                cancel.cancel();
-                return;
-            }
-        };
-        while let Some(message) = stream.next().await {
-            let _ = match send_email(&message).await {
-                Err(e) => {
-                    tracing::error!("Error sending email: {:?}", e);
-                    cancel.cancel();
-                    message.nack().await
-                }
-                Ok(_) => {
-                    tracing::info!("Email sent successfully");
-                    message.ack().await
-                }
-            };
-        }
-    });
-
-    // Receive blocks until the ctx is cancelled or an error occurs.
-    // Or simply use the `subscription.subscribe` method.
-    // subscription
-    //     .receive(
-    //         |message, cancel| async move {
-    //             let _ = match send_email(&message).await {
-    //                 Err(e) => {
-    //                     println!("Error sending email: {:?}", e);
-    //                     cancel.cancel();
-    //                     message.nack().await
-    //                 }
-    //                 Ok(_) => {
-    //                     println!("Email sent successfully");
-    //                     message.ack().await
-    //                 }
-    //             };
-    //         },
-    //         cancel.clone(),
-    //         None,
-    //     )
-    //     .await?;
-
-    // Delete subscription if needed.
-    // subscription.delete(None).await?;
-
     Ok(())
 }
